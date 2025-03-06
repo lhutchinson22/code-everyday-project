1cd9a404e44b3ae1643e163accdee5955c02f49d
Lauren Hutchinson <lbhutchinson022@gmail.com>
2025-03-06 14:01:45 -0800

    error fixed

--- website/views.py
+++ website/views.py
@@ -1,8 +1,6 @@
 from flask import Blueprint, render_template
 from flask_login import UserMixin, login_user, login_required, logout_user, current_user
-from . import create_app  # Import the create_app function
-
-app = create_app()  # Initialize the app
+from . import login_manager  # Import the login_manager instance
 
 views = Blueprint('views', __name__)
 
@@ -14,7 +12,7 @@ class User(UserMixin):
         # For example, you can query the database to get the user
         return None  # Replace with actual user retrieval logic
 
-@app.login_manager.user_loader
+@login_manager.user_loader
 def load_user(user_id):
     # Implement the logic to load a user given the user_id
     return User.get(user_id)
