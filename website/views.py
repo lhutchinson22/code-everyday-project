4ea9c5eaf38b17a1f10b967ce2c27c9d05eb59a9
Lauren Hutchinson <lbhutchinson022@gmail.com>
2025-03-18 14:13:54 -0700

    added input fields in home page to be able to select city, month, and year

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
@@ -10,7 +10,23 @@ def index():
     else:
         return redirect(url_for('auth.login'))
 
-@views.route('/home')
+@views.route('/home', methods=['GET', 'POST'])
 @login_required
 def home():
-    return render_template('home.html')
+    city = None
+    month = None
+    year = None
+    
+    if request.method == 'POST':
+        city = request.form.get('city')
+        month = request.form.get('month')
+        year = request.form.get('year')
+        
+        if not city:
+            flash('City is required!', category='error')
+        if not month:
+            flash('Month is required!', category='error')
+        if not year:
+            flash('Year is required!', category='error')
+            
+    return render_template('home.html', city=city, month=month, year=year)
